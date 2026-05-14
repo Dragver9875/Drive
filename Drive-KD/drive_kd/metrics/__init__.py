2f37f779c16037913896db4608206cf581444fa3
Piyush <pandapiyush086@gmail.com>
2026-05-14 22:24:56 +0530

    Update __init__.py

--- Drive-KD/drive_kd/metrics/__init__.py
+++ Drive-KD/drive_kd/metrics/__init__.py
@@ -1,5 +1,9 @@
 from drive_kd.metrics.boundary_metrics import BoundaryMetrics, boundary_counts, boundary_metrics_from_counts
-from drive_kd.metrics.efficiency_metrics import count_parameters, estimate_model_size_mb
+from drive_kd.metrics.efficiency_metrics import (
+    benchmark_fps,
+    count_parameters,
+    estimate_model_size_mb,
+)
 from drive_kd.metrics.evaluator import DriveEvaluator
 from drive_kd.metrics.lane_metrics import LaneMetrics, lane_counts, lane_metrics_from_counts
 from drive_kd.metrics.road_metrics import RoadMetrics, road_counts, road_metrics_from_counts
@@ -29,4 +33,5 @@ __all__ = [
     "DriveEvaluator",
     "count_parameters",
     "estimate_model_size_mb",
+    "benchmark_fps",
 ]
